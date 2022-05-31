23d80628db23143e78b3e5213646035c5dfcb321
Ahmed Mahmoud <ahmahmoud@ucdavis.edu>
2022-05-31 08:37:11 -0400

    setup

--- shmem/include/main.cu
+++ shmem/include/main.cu
@@ -3,8 +3,6 @@
 #include <stdio.h>
 #include "gtest/gtest.h"
 
-#include "CUDALib.h"
-
 __global__ void exec_kernel()
 {
     printf("\n I am thread %d from exec_kernel\n", threadIdx.x);
@@ -15,14 +13,6 @@ TEST(Test, exe)
     exec_kernel<<<1, 1>>>();
     auto err = cudaDeviceSynchronize();
     EXPECT_EQ(err, cudaSuccess);    
-}
-
-TEST(Test, lib)
-{
-    CUDALib lib;
-    lib.run();
-    auto err = cudaDeviceSynchronize();
-    EXPECT_EQ(err, cudaSuccess);
 }
 
 int main(int argc, char** argv)
